d8496127d67a008cdce00a2e81b7f0d714f21b51
jhgiPhone <jgoode@jhgmedia.net>
2011-09-04 10:04:17 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contacts::Application.config.secret_token = 'dd282257155a27062e2c2d0eb126e46329c9a564b869fae7eb60489274b420678f5365ff559e86fc86f40c3f80ca62a5181cdccdcddf09a796566ab74ef6a566'
